86796dd4e44fd24cb8859ccbb0c89a9a7c3f94ea
nicolas1897 <106556299+nicolas1897@users.noreply.github.com>
2022-12-02 11:45:35 -0500

    updates

--- models/city.py
+++ models/city.py
@@ -1,23 +1,24 @@
-#!/usr/bin/python3
-"""
-City module
-"""
+#!/usr/bin/python
+""" holds class City"""
+import models
 from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
-    """
-    Class City
-    """
+    """Representation of city """
+    if models.storage_t == "db":
+        __tablename__ = 'cities'
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        name = Column(String(128), nullable=False)
+        places = relationship("Place", backref="cities")
+    else:
+        state_id = ""
+        name = ""
 
-    __tablename__ = "cities"  #TODO change simple attr to table cities
-    state_id = Column(
-        String(60),
-        nullable=False,
-        ForeignKey('state.id')
-    )
-    name = Column(
-        String(128),
-        nullable=False
-    )
+    def __init__(self, *args, **kwargs):
+        """initializes city"""
+        super().__init__(*args, **kwargs)
