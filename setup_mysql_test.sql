28f6a68c506270e628f6d7e0f46276450888efae
nicolas1897 <106556299+nicolas1897@users.noreply.github.com>
2022-12-02 11:30:45 -0500

    updates

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,4 +1,7 @@
--- prepares a MySQL server for the tests
+-- prepares a MySQL server for the project
+
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
