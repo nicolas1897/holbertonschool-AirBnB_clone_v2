b9dc121a5e62a1e9212df19f16b256af60476ef0
nicolas1897 <106556299+nicolas1897@users.noreply.github.com>
2022-12-02 11:14:26 -0500

    updates

--- models/city.py
+++ models/city.py
@@ -1,9 +1,23 @@
 #!/usr/bin/python3
-""" City Module for HBNB project """
-from models.base_model import BaseModel
+"""
+City module
+"""
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
 
 
-class City(BaseModel):
-    """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+class City(BaseModel, Base):
+    """
+    Class City
+    """
+
+    __tablename__ = "cities"  #TODO change simple attr to table cities
+    state_id = Column(
+        String(60),
+        nullable=False,
+        ForeignKey('state.id')
+    )
+    name = Column(
+        String(128),
+        nullable=False
+    )
