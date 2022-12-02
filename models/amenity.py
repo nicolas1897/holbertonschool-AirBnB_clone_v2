486d758bf130355bfebd9e44ebfe112284cb03b9
nicolas1897 <106556299+nicolas1897@users.noreply.github.com>
2022-12-02 11:44:31 -0500

    updates

--- models/amenity.py
+++ models/amenity.py
@@ -1,18 +1,21 @@
-#!/usr/bin/python3
-"""
-Amenity module
-"""
+#!/usr/bin/python
+""" holds class Amenity"""
+import models
 from models.base_model import BaseModel, Base
-from sqlalquemy import Column, String
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel, Base):
-    """
-    Class Amenity
-    """
+    """Representation of Amenity """
+    if models.storage_t == 'db':
+        __tablename__ = 'amenities'
+        name = Column(String(128), nullable=False)
+    else:
+        name = ""
 
-    __tablename__ = 'amenities' #TODO change simple attr to table amenities
-    name = Column(
-        String(128),
-        nullable=False
-    )
+    def __init__(self, *args, **kwargs):
+        """initializes Amenity"""
+        super().__init__(*args, **kwargs)
