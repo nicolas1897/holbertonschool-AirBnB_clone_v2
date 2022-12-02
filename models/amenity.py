1898d2d41befb7a747081cada31094bfc29c06f0
nicolas1897 <106556299+nicolas1897@users.noreply.github.com>
2022-12-02 11:13:03 -0500

    updates

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+"""
+Amenity module
+"""
+from models.base_model import BaseModel, Base
+from sqlalquemy import Column, String
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """
+    Class Amenity
+    """
+
+    __tablename__ = 'amenities' #TODO change simple attr to table amenities
+    name = Column(
+        String(128),
+        nullable=False
+    )
